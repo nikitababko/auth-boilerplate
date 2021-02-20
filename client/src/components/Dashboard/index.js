f680365989ed06312fb98cc590948ebbb7b69b42
Nikita Babko <niki-babko@yandex.ru>
2021-02-20 17:38:44 +0700

    Added components: Dashboard and Preferences.

---
+++ client/src/components/Dashboard/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const index = () => {
+  return <h2>Dashboard</h2>;
+};
+
+export default index;
---
+++ client/src/components/Preferences/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const index = () => {
+  return <h2>Preferences</h2>;
+};
+
+export default index;
